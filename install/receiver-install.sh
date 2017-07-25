c7bc97c822b66b791fcc292f983e5aa175d64296
Steve Norum <stevenorum@gmail.com>
2017-07-25 01:02:29 -0400

    Fix LED cache and other random stuff.

--- install/receiver-install.sh
+++ install/receiver-install.sh
@@ -10,9 +10,11 @@ cd ${INSTALLDIR}/pyropossum-master
 pip3 install -r ${INSTALLDIR}/pyropossum-master/requirements.txt
 python3 ${INSTALLDIR}/pyropossum-master/setup.py install
 cp ${INSTALLDIR}/pyropossum-master/etc/init.d/pyropossum /etc/init.d/pyropossum
+cp ${INSTALLDIR}/pyropossum-master/etc/cron.d/pyropossum /etc/cron.d/pyropossum
 mkdir -p /etc/pyropossum
 cp ${INSTALLDIR}/pyropossum-master/config.json /etc/pyropossum/config.json
 chmod +x /etc/init.d/pyropossum
+chmod +x /etc/cron.d/pyropossum
 # For some reason this isn't currently working through /usr/sbin/service, so make the raw call instead.
 /etc/init.d/pyropossum start
 cd $HERE
